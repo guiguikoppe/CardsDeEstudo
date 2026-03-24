271c932f4dc712d6a77f220ce5ea8b17f6b0fe24
dav1zera24 <davi24san@gmail.com>
2026-03-24 08:19:40 -0300

    mudanças teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import AutoCard from './components/AutorCard/AutoCard';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <AutoCard 
+        titulo="React"
+        explicacao="React é uma biblioteca JavaScript para criar interfaces de usuário."
+        exemploC="const element = <h1>Hello, world!</h1>;"
+        dica="Comece com o tutorial oficial do React."
+      />
+    </div>
   )
 }
 
